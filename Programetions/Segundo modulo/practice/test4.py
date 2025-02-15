79520a83cf4259aa4113b7fb97621d8f387fb845
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-02-15 14:42:12 -0300

    Add files via upload

---
+++ Programetions/Segundo modulo/practice/test4.py
@@ -0,0 +1,16 @@
+class Animal:
+    def falar(self):
+        print("Este animal faz um som.")
+
+class Cachorro(Animal):
+    def falar(self):
+        print("O cachorro late.")
+
+class Gato(Animal):
+    def falar(self):
+        print("O gato mia.")
+
+# Exemplo de uso
+animais = [Cachorro(), Gato(), Animal()]
+for animal in animais:
+    animal.falar()
