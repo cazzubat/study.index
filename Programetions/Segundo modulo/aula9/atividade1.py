3fcee0a3534d09ba8eaa75ca1b90e4a5747e6647
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-02-15 10:29:48 -0300

    Add files via upload

---
+++ Programetions/Segundo modulo/aula9/atividade1.py
@@ -0,0 +1,40 @@
+class Livro:
+    def __init__(self, titulo, autor, ano, categoria, editora, ISBN):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.categoria = categoria
+        self.editora = editora
+        self.ISBN = ISBN
+        
+    def __str__(self):
+        return f'{self.titulo} | {self.autor} | {self.ano} | {self.categoria} | {self.editora} | {self.ISBN} '
+
+class Estante:
+    def __init__(self, didaticos, literatura, cronica, romcance, terror):
+        self.ditaticos = didaticos
+        self.literaturas = literatura
+        self.cronica = cronica
+        self.romance = romcance
+        self.terror = terror
+
+class Usuario:
+    def __init__(self, nome, cpf, identidade):
+        self.nome = nome
+        self.cpf = cpf
+        self.identidade = identidade
+
+class Funcionario:
+    def __init__(self, id, nome_funcionario, senha):
+        self.nome_funcionario = nome_funcionario
+        self.id = id
+        self.senha = senha
+        
+
+class Biblioteca:
+    def __init__(self, endereço, nome_biblioteca):
+        self.endereço = endereço
+        self.nome_biblioteca = nome_biblioteca
+
+memorias = Livro('Memórias póstumas', 'Machadão', '1898', 'romance', 'primeiro de abril', '01A')
+print(memorias)
