e03b783df95ee2ac47d578dcf49ccd42a2534e09
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-03-20 21:04:25 -0300

    Update atividade1.py

--- Programetions/Segundo modulo/aula12/atividade1.py
+++ Programetions/Segundo modulo/aula12/atividade1.py
@@ -1,1 +1,11 @@
-#Oi, tudo bem? Será que você pode me ajudar a criar um sistema para minha loja? Gostaria de algo onde eu pudesse visualizar quem fez os pedidos e acessar o cardápio de forma fácil.
+#Oi, tudo bem? Será que você pode me ajudar a criar um sistema para minha loja? 
+# Gostaria de algo onde eu pudesse visualizar quem fez os pedidos e acessar o cardápio de forma fácil.
+
+class restaurante:
+    pass
+
+class client:
+    pass
+
+class funcinario:
+    pass
