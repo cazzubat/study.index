8fa892ade849f75ce42ea24459f1fe5073ba80a0
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-03-22 10:24:57 -0300

    Update atividade1.py

--- Programetions/Segundo modulo/aula13/atividade1.py
+++ Programetions/Segundo modulo/aula13/atividade1.py
@@ -1,6 +1,14 @@
 import flet as ft 
  
 def main(page:ft.Page):
-    ola = ft.Text('Olá mundo!',size=50)
-    page.add(ola)
+    # page.theme = "dark"   
+    def handle_click(event):
+        page.add(ft.Text("Cliquei no caiox"))
+        # page.update()
+
+    ola = ft.Text("Fala que é noix!",size=50,color="red",bgcolor="yellow")
+    botao = ft.ElevatedButton(text="Clica no caiox",on_click=handle_click)
+
+    page.add(ola,botao)
+
 ft.app(target=main)
