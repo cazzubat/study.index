afdf4bfb0686658b144447811c5ef1f26b17b345
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-03-29 08:28:41 -0300

    Add files via upload

---
+++ Programetions/Segundo modulo/aula14/cmd_ambiente_virtual.py
@@ -0,0 +1,15 @@
+# Linux:
+# 1. Instalar Python 3: sudo apt install python3 python3-pip python3-venv
+# 2. Criar ambiente virtual: python3 -m venv nome_do_ambiente
+# 3. Ativar ambiente virtual: source nome_do_ambiente/bin/activate
+# 4. Desativar ambiente virtual: deactivate
+
+# Windows (Prompt de Comando):
+# 1. Criar ambiente virtual: python -m venv nome_do_ambiente
+# 2. Ativar ambiente virtual: nome_do_ambiente\Scripts\activate
+# 3. Desativar ambiente virtual: deactivate
+
+# Windows (PowerShell):
+# 1. Criar ambiente virtual: python -m venv nome_do_ambiente
+# 2. Ativar ambiente virtual: .\nome_do_ambiente\Scripts\Activate
+# 3. Desativar ambiente virtual: deactivate
