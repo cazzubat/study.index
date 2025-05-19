40bb5e9df0ea70cc2855878af57dfd0c99a2e6c3
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-05-19 18:02:43 -0300

    Add files via upload

---
+++ Python API/aula2/app.py
@@ -0,0 +1,20 @@
+#app.py
+
+from flask import Flask, request, jsonify, abort
+from blueprints.items import bp as items_bp
+
+app = Flask(__name__)
+app.register_blueprint(items_bp)
+
+@app.route('/') #decorator
+def index():
+    return 'Hello, Flask!'
+
+@app.route('/ping')
+def ping():
+    data = {'message': 'pong'}
+    return jsonify(data), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
