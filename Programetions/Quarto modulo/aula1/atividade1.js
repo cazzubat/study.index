1cbede8c405bee3c03e608da3c6c36a123d06a91
cazzu <66221633+cazzubat@users.noreply.github.com>
2025-07-12 09:54:39 -0300

    Add files via upload

---
+++ Programetions/Quarto modulo/aula1/atividade1.js
@@ -0,0 +1,7 @@
+let bosta = 'Bosta'
+let mijo = 'mijo'
+const comprimento = 'Olá,'
+let comprimentoMaster = comprimento + bosta + mijo
+
+console.log(comprimentoMaster)
+console.log('Quantidade de caracter:', comprimentoMaster.length)
